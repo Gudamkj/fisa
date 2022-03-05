dbd8bb45872a2d3f8174f6c1ffd5a3d295f9b80f
guda <3070940143@qq.com>
2022-03-05 20:34:55 +0800

    first-commit

--- src/main/java/cy/xianqi/fdher.java
+++ src/main/java/cy/xianqi/fdher.java
@@ -3,5 +3,6 @@ package cy.xianqi;
 public class fdher {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("second hello!");
     }
 }
